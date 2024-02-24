fe95c7f9010c29f1727e83d34878296a5e599814
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-24 22:00:25 +0530

    day 10 lecture 1 completed

--- app/src/main/java/com/example/day10lecture1/MainActivity.kt
+++ app/src/main/java/com/example/day10lecture1/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.day10lecture1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val gotoNextActivity = findViewById<Button>(R.id.go_to_next_page)
+        gotoNextActivity.setOnClickListener {
+            // explicit intent
+            val intent = Intent(this , NextActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        val toastButton = findViewById<Button>(R.id.button2)
+        toastButton.setOnClickListener {
+            val toast = Toast.makeText(this , "What is your name bharat" , Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
